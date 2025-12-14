2f90bf388cfe1423027d755996ac07024cfc00ad
ArpitShukla3 <arpit900shukla@gmail.com>
2025-12-14 13:09:28 +0530

    setup frontend (zustand + shadcn + tailwind 1st push

---
+++ src/stores/modelStore.ts
@@ -0,0 +1,8 @@
+import type { BearType } from '@myTypes/globalTypes'
+import { create } from 'zustand'
+const useBear = create((set) => ({
+  bears: 0,
+  increasePopulation: () => set((state : any) => ({ bears: state.bears + 1 })),
+  removeAllBears: () => set({ bears: 0 }),
+  updateBears: (newBears: BearType) => set({ bears: newBears }),
+}))
---
+++ src/types/globalTypes.ts
@@ -0,0 +1,3 @@
+export type BearType ={
+    id: number;
+}
